e96156c90437129695d4e1d2fbff025013878c6b
Jonathan Alumbaugh <jalumbaugh@duffyshanley.com>
2020-09-15 08:26:56 -0400

    Breakout footer to component

---
+++ src/components/footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer accent">
+      <div className="w-container">
+        <p className="paragraph">
+          The place a footer would normally tell you that this stuff belonged to
+          one person or company.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
