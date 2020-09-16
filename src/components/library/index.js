84aa6f4923ed52b44b971d9a73126d1b9856e208
Jonathan Alumbaugh <jalumbaugh@duffyshanley.com>
2020-09-16 19:38:29 -0400

    Add animation to library
    Attempted to use staggerChildren but doesn't appear to work on react components

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 import library from '../../shared/doucette-media.json'
 
@@ -6,17 +7,31 @@ import Clip from '../clip'
 import './Library.scss'
 
 function Library() {
+  const variants = {
+    visible: {
+      transition: {
+        when: 'beforeChildren',
+        // staggerChildren: 0.1,
+      },
+    },
+  }
+
   return (
     <div className="police-brutality">
       <div className="collection-list-wrapper w-dyn-list">
-        <div className="collection-list w-dyn-items">
+        <motion.div
+          className="collection-list w-dyn-items"
+          animate="visible"
+          variants={variants}
+          layout
+        >
           {library
-            ?.filter((i) => i.Video['Image Filename'].match(/.mp4/))
+            .filter((i) => i.Video['Image Filename'].match(/.mp4/))
             .slice(0, 40)
             .map((item, i) => {
               return <Clip item={item} i={i} key={item['TGD Number']} />
             })}
-        </div>
+        </motion.div>
 
         {!library && (
           <div className="w-dyn-empty">
