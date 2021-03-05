5fb8c2ec448db1e45c1350b1b91878fa40f71740
Jonathan Alumbaugh <jonathan.alumbaugh@gmail.com>
2021-03-05 16:12:42 -0500

    Overhaul paging

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -118,36 +118,71 @@ export default ({ match }) => {
   return (
     <div className="police-brutality">
       <div className="collection-list-wrapper w-dyn-list">
-        <button
-          onClick={() => {
-            pageNumber > 1 ? setPageNumber(pageNumber - 1) : setPageNumber(1)
-          }}
-        >
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </button>
-        {Array.apply(null, { length: totalPages }).map((p, i) => {
-          return (
+        <div className="paging-controls">
+          <button
+            className="paging-button"
+            onClick={() => {
+              pageNumber > 1 ? setPageNumber(pageNumber - 1) : setPageNumber(1)
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <div className="paging-numbers">
             <Link
-              to={{ pathname: '/', pageId: `${i + 1}` }}
-              key={i}
+              className="paging-number"
+              to={{ pathname: '/', pageId: 1 }}
+              key={0}
               onClick={() => {
-                setPageNumber(i + 1)
+                setPageNumber(1)
               }}
-              style={{ margin: '0.2em' }}
             >
-              {i + 1}
+              1
             </Link>
-          )
-        })}
-        <button
-          onClick={() => {
-            pageNumber < totalPages
-              ? setPageNumber(pageNumber + 1)
-              : setPageNumber(totalPages)
-          }}
-        >
-          <FontAwesomeIcon icon={faChevronRight} />
-        </button>
+
+            <hr className="paging-number-divider" />
+
+            {Array.apply(null, { length: totalPages }).map((p, i) => {
+              if (i !== 0 && i < pageNumber + 3 && i > pageNumber - 3) {
+                return (
+                  <Link
+                    className="paging-number"
+                    to={{ pathname: '/', pageId: `${i + 1}` }}
+                    key={i}
+                    onClick={() => {
+                      setPageNumber(i + 1)
+                    }}
+                  >
+                    {i + 1}
+                  </Link>
+                )
+              }
+            })}
+
+            <hr className="paging-number-divider" />
+
+            <Link
+              className="paging-number"
+              to={{ pathname: '/', pageId: totalPages }}
+              key={totalPages}
+              onClick={() => {
+                setPageNumber(totalPages)
+              }}
+            >
+              {totalPages}
+            </Link>
+          </div>
+          <button
+            className="paging-button"
+            onClick={() => {
+              pageNumber < totalPages
+                ? setPageNumber(pageNumber + 1)
+                : setPageNumber(totalPages)
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+
         <div className="collection-list w-dyn-items">
           <List cardData={cardData} match={match} />
         </div>
